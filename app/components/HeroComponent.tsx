a99c44baad98a65af650200cba693f5d18f1772c
Aidan <ayounathan05@gmail.com>
2026-03-02 13:06:37 -0800

    gulp

--- app/components/HeroComponent.tsx
+++ app/components/HeroComponent.tsx
@@ -2,9 +2,10 @@
 
 const HeaderComponent = () => {
     return (
-        <div className="">
-            <div className="">
+        <div className=" flex px-56 border-8 border-amber-200">
+            <div className="text-[64px] font-bold  text-var(--foreground)">
                 <h1>Aidan <br /> Younathan</h1>
+                <h4 className="text-2xl text-[#555954]">Software engineer building end-to-end systems with a focus on accessibility, reliability, and maintainable architecture.</h4>
             </div>
         </div>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import HeaderComponent from "./components/HeaderComponent";
+import HeroComponent from "./components/HeroComponent";
 
 
 export default function Home() {
   return (
     <main>
       <HeaderComponent />
+      <HeroComponent />
     </main>
   );
 }
