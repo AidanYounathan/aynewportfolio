0094f1bb6fe1f730aca736058f301fa3fe6c1e4d
Aidan <ayounathan05@gmail.com>
2026-03-02 17:20:48 -0800

    Enhance font styling for consistency in Header and Hero components

--- app/components/HeroComponent.tsx
+++ app/components/HeroComponent.tsx
@@ -2,10 +2,10 @@
 
 const HeaderComponent = () => {
     return (
-        <div className=" flex px-56 border-8 border-amber-200">
+        <div className=" flex px-56 border-8 border-amber-200 font-[family-name:var(--font-manrope)]">
             <div className="text-[64px] font-bold  text-var(--foreground)">
                 <h1>Aidan <br /> Younathan</h1>
-                <h4 className="text-2xl text-[#555954]">Software engineer building end-to-end systems with a focus on accessibility, reliability, and maintainable architecture.</h4>
+                <h4 className="text-2xl font-semibold text-[#555954]">Software engineer building end-to-end systems with a focus on accessibility, reliability, and maintainable architecture.</h4>
             </div>
         </div>
     )
